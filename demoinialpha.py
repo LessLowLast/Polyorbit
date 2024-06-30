9321a79e686dde1ae68cf45048c4b0a5e4d1a52e
LessLowLast <notgoodatlying@gmail.com>
2024-06-30 04:10:31 -0400

    Add files via upload
    Update includes
    Suno Recording (19 second incremental recording) 
    Selectable dynamic INI files so you can save your work, or share your works with others.

--- demoinialpha.py
+++ demoinialpha.py
@@ -3,6 +3,8 @@ import math
 import os
 from pyo import *
 import configparser
+import glob
+import pygame_gui
 
 # Initialize Pygame
 pygame.init()
@@ -126,9 +128,9 @@ class Moon(CelestialBody):
         # Draw moon
         pygame.draw.circle(screen, BLUE, (x, y), int(max(self.size * zoom_level, 1)))
 
-def load_settings():
+def load_settings(file):
     config = configparser.ConfigParser()
-    config.read('settings.ini')
+    config.read(file)
     
     planets = []
     
@@ -167,7 +169,40 @@ def load_settings():
     
     return planets, speed_multiplier
 
-planets, GLOBAL_SPEED_MULTIPLIER = load_settings()
+def start_recording():
+    global is_recording, record_start_time, record_counter
+    is_recording = True
+    record_start_time = pygame.time.get_ticks()
+    filename = f"recording_{record_counter}.wav"
+    s.recstart(filename)
+
+def stop_recording():
+    global is_recording, record_counter
+    is_recording = False
+    s.recstop()
+    record_counter += 1
+
+# Load planets from settings.ini
+planets, GLOBAL_SPEED_MULTIPLIER = load_settings('settings.ini')
+
+# GUI setup
+manager = pygame_gui.UIManager((WIDTH, HEIGHT))
+
+# Dropdown for .ini file selection
+ini_files = glob.glob('*.ini')
+dropdown = pygame_gui.elements.UIDropDownMenu(
+    options_list=ini_files,
+    starting_option='settings.ini',
+    relative_rect=pygame.Rect((10, 10), (200, 30)),
+    manager=manager
+)
+
+# Button for sound recording
+record_button = pygame_gui.elements.UIButton(
+    relative_rect=pygame.Rect((220, 10), (150, 30)),
+    text='Output for Suno',
+    manager=manager
+)
 
 running = True
 clock = pygame.time.Clock()
@@ -176,7 +211,14 @@ zoom_level = 1.0
 min_zoom = 0.01  # Allows zooming out 100 times
 max_zoom = 10.0  # Allows zooming in 10 times
 
+# Recording variables
+is_recording = False
+record_start_time = 0
+record_counter = 0
+RECORDING_DURATION = 19000  # 19 seconds in milliseconds
+
 while running:
+    time_delta = clock.tick(60) / 1000.0
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -187,6 +229,30 @@ while running:
             elif event.button == 5:  # Scroll down
                 zoom_level /= 1.1
                 zoom_level = max(zoom_level, min_zoom)
+        elif event.type == pygame.USEREVENT:
+            if event.user_type == pygame_gui.UI_DROP_DOWN_MENU_CHANGED:
+                if event.ui_element == dropdown:
+                    selected_file = event.text
+                    planets, GLOBAL_SPEED_MULTIPLIER = load_settings(selected_file)
+            if event.user_type == pygame_gui.UI_BUTTON_PRESSED:
+                if event.ui_element == record_button:
+                    if is_recording:
+                        stop_recording()
+                        record_button.set_text('Output for Suno')
+                    else:
+                        start_recording()
+                        record_button.set_text('Stop Recording')
+
+        manager.process_events(event)
+
+    manager.update(time_delta)
+
+    # Check if recording needs to be stopped and restarted
+    if is_recording:
+        current_time = pygame.time.get_ticks()
+        if current_time - record_start_time >= RECORDING_DURATION:
+            stop_recording()
+            start_recording()
 
     screen.fill(BLACK)
 
@@ -221,8 +287,20 @@ while running:
     # Draw center
     pygame.draw.circle(screen, WHITE, CENTER, 5)
 
+    # Update button color based on recording state
+    if is_recording:
+        record_button.colours['normal_bg'] = pygame.Color('red')
+        record_button.colours['hovered_bg'] = pygame.Color('darkred')
+        record_button.colours['active_bg'] = pygame.Color('darkred')
+    else:
+        record_button.colours['normal_bg'] = pygame.Color('#45494e')
+        record_button.colours['hovered_bg'] = pygame.Color('#35393e')
+        record_button.colours['active_bg'] = pygame.Color('#35393e')
+    record_button.rebuild()
+
+    manager.draw_ui(screen)
+
     pygame.display.flip()
-    clock.tick(60)
 
 # Clean up
 s.stop()
